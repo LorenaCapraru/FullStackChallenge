7702a18f73ec21780556690e4354dd02b6b0a895
Lorena <lorenacapraru@gmail.com>
2023-09-25 11:43:30 +0100

    Add Shops page into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Shops from "./pages/Shops/Shops";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Shops />
+    </div>
+  );
 }
 
 export default App;
