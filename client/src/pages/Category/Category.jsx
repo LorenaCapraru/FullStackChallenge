77f5758c46c09c16ec3f9db21b715150090b2b34
Lorena <lorenacapraru@gmail.com>
2023-09-25 14:07:31 +0100

    Create Category

---
+++ client/src/pages/Category/Category.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import CategoryCard from "../../components/CategoryCard";
+const Category = () => {
+  const { shopId } = useParams();
+  const [categories, setCategories] = useState([]);
+  const [load, setLoading] = useState(true);
+
+  useEffect(() => {
+    const category = `https://full-stack-challenge-klt3.onrender.com/store${Number(
+      shopId
+    )}-category`;
+    const fetchCategory = async () => {
+      try {
+        const response = await fetch(category);
+        if (!response.ok) {
+          throw new Error("Network response not ok.");
+        }
+        const data = await response.json();
+        setCategories(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("error while fetching data", error);
+        setLoading(true);
+      }
+    };
+    fetchCategory();
+  }, [shopId]);
+  console.log(categories);
+  return (
+    <div className="categoriesContainer">
+      {load ? (
+        <p>is loading </p>
+      ) : (
+        categories.map((category) => (
+          <CategoryCard shopId={shopId} category={category} />
+        ))
+      )}
+    </div>
+  );
+};
+export default Category;
