fbc359bbb865a85631f72731c5d6a6a759a971fe
Lorena <lorenacapraru@gmail.com>
2023-09-25 14:06:57 +0100

    Creating Category

---
+++ client/src/components/CategoryCard.jsx
@@ -0,0 +1,19 @@
+import "./CategoryCard.css";
+import { Link } from "react-router-dom";
+
+const CategoryCard = ({ category, shopId }) => {
+  return (
+    <div className="shopCard">
+      <Link to={`/shop/${shopId}/items`}>
+        <img src={category.img} alt="category description" />
+        <p>
+          {category.name
+            .split("")
+            .map((el) => el.toUpperCase())
+            .join("")}
+        </p>
+      </Link>
+    </div>
+  );
+};
+export default CategoryCard;
