cb1a52495c07a0ef44e9e8858ae1dd8183f5fefc
Lorena <lorenacapraru@gmail.com>
2023-09-25 14:06:51 +0100

    Added new components and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import Shops from "./pages/Shops/Shops";
+import Category from "./pages/Category/Category";
+import Items from "./pages/Items/Items";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Shops />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Shops />} />
+        <Route path="/shop/:shopId" element={<Category />} />
+        <Route path="/shop/:shopId/items" element={<Items />} />
+
+        {/* <Link to={`/items/${category.id}/`}> */}
+      </Routes>
+    </Router>
   );
 }
 
