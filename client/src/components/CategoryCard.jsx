62eaa9211c40e4a38a4f2c57bb7f6444a8e4e6b6
Lorena <lorenacapraru@gmail.com>
2023-09-27 13:17:27 +0100

    Update CategoryCard.jsx

--- client/src/components/CategoryCard.jsx
+++ client/src/components/CategoryCard.jsx
@@ -1,9 +1,10 @@
 import "./CategoryCard.css";
+import "./ShopCard.css";
 import { Link } from "react-router-dom";
 
-const CategoryCard = ({ category, shopId }) => {
+const CategoryCard = ({ category, shopId, click }) => {
   return (
-    <div className="shopCard">
+    <div className={click === true ? "clickCardTrue" : "shopCard"}>
       <Link to={`/shop/${shopId}/${category.id}`}>
         <img src={category.img} alt="category description" />
         <p>
